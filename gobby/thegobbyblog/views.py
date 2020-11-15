24e2f5dce2343a6a0432017be5dc3e3e62e3883a
Lylio <cxz@gmx.com>
2020-11-15 00:26:15 +0000

    update

--- gobby/thegobbyblog/views.py
+++ gobby/thegobbyblog/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 
 class ArticleDetailView(DetailView):
@@ -18,7 +20,14 @@ class AddPostView(CreateView):
     form_class = PostForm
     template_name = 'add_post.html'
 
+
 class UpdatePostView(UpdateView):
     model = Post
     form_class = EditForm
     template_name = 'update_post.html'
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
