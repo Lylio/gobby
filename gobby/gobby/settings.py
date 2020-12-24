95a6d1562c682bd7a7fcd8133fba920859a3afdb
Lylio <cxz@gmx.com>
2020-12-24 16:19:24 +0000

    docker-compose update

--- gobby/gobby/settings.py
+++ gobby/gobby/settings.py
@@ -83,6 +83,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'postgres',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': 'db',
+#        'PORT': 5432,
+#    }
+#}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
